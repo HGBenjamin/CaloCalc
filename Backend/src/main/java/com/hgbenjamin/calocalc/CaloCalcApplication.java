57245f1979a189da6200de4aaaf7f01dd1fa6f21
HGBenjamin <benjamin.haivan@gmail.com>
2025-12-03 22:45:03 -0500

    Correct backend folder structure for project.
    - The previous folder structure for the back end was not compatible with maven.
    - Added an example .env so users can test on their machines.

---
+++ Backend/src/main/java/com/hgbenjamin/calocalc/CaloCalcApplication.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.hgbenjamin.calocalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author haivanbenjamin
+ */
+@SpringBootApplication
+public class CaloCalcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaloCalcApplication.class, args);
+    }
+}
