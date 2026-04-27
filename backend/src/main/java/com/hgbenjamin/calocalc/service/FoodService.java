dd489965aad31b6840b60920d7e67d3ca1416e56
HGBenjamin <benjamin.haivan@gmail.com>
2026-04-27 00:22:47 -0400

    Feat: implement basic food service class

--- backend/src/main/java/com/hgbenjamin/calocalc/service/FoodService.java
+++ backend/src/main/java/com/hgbenjamin/calocalc/service/FoodService.java
@@ -1,12 +1,46 @@
 package com.hgbenjamin.calocalc.service;
 
+import com.hgbenjamin.calocalc.entity.Food;
+import com.hgbenjamin.calocalc.repository.FoodRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
 /**
  * This class is responsible for the business logic of the DailyLogs class. 
  * 
  * 
  * @author haivanbenjamin
  */
+@Service
 public class FoodService 
 {
+    private final FoodRepository foodRepository;
+
+    public FoodService(FoodRepository foodRepository)
+    {
+        this.foodRepository = foodRepository;
+    }
+
+    public List<Food> findAllByName(String query)
+    {
+        // check database to see if the foods exist
+        
+        // if it does return that, user may not see their choice and choose to have a wider search (use usda db)
+        
+        // otherwise check usda db 
+        
+        // store the results in server db
+        
+        // return the results to the user
+        
+        
+        
+        return foodRepository.findByNameContainingIgnoreCase(query);
+    }
+
+    public List<Food> listFoods()
+    {
+        return foodRepository.findAll();
+    }
     
 }
